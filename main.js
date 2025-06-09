a7b9fc0266947b1f15aeefd03ec98c771bf26fe5
Anton <gendra.job@gmail.com>
2025-06-09 10:42:06 +0200

    Use another selector if link is not found using the original

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let target = null;
 
 const clickTarget = () => {
   target = document.querySelector('[data-testid="dismiss-welcome"]');
+
+  if (!target) {
+    target = document.querySelector('[data-testid="modal-no-auth-rate-limit"] a[href="#"]');
+  }
   if (!target) {
     setTimeout(clickTarget, 200);
   } else {
